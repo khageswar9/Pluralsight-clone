2ee0761ec90d1bec5130291c5716a651b2236d4f
Viraj Gupta <vgupta2@q3infotech.net>
2023-09-03 23:53:14 +0530

    fix: remove node modules and hide secret sources

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = () =>{
-    mongoose.connect("mongodb+srv://virajgupta:Virajgupta12345@cluster0.wmplk.mongodb.net/npl?retryWrites=true&w=majority");
+    mongoose.connect(`mongodb+srv://${env.SECRET_KEY}@cluster0.wmplk.mongodb.net/npl?retryWrites=true&w=majority`);
 };
